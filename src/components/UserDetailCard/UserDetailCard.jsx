491701279af9170bcfdb69c29f7f493dace7171d
Alan <alancurtis5000+accounts@gmail.com>
2021-02-17 00:40:15 -0700

    fixed warning

--- src/components/UserDetailCard/UserDetailCard.jsx
+++ src/components/UserDetailCard/UserDetailCard.jsx
@@ -33,34 +33,34 @@ const UserDetailCard = (props) => {
         <Skeleton loading={false} avatar active>
           <Meta avatar={<Avatar src={picture} />} title={name} />
           <div style={{ marginTop: 16 }}>
-            <label for="email">Email: </label>
+            <label htmlFor="email">Email: </label>
             <span id="email">{email}</span>
           </div>
           <div>
-            <label for="phone">Phone: </label>
+            <label htmlFor="phone">Phone: </label>
             <span id="phone">{phone}</span>
           </div>
           <div>
-            <label for="address">Address: </label>
+            <label htmlFor="address">Address: </label>
             <span id="address">{address}</span>
           </div>
 
           <div>
-            <label for="city">City: </label>
+            <label htmlFor="city">City: </label>
             <span id="city">{city}</span>
           </div>
           <div>
-            <label for="postcode">Postcode: </label>
+            <label htmlFor="postcode">Postcode: </label>
             <span id="postcode">{postcode}</span>
           </div>
           <div>
-            <label for="country">Country: </label>
+            <label htmlFor="country">Country: </label>
             <span id="country">{country}</span>
           </div>
 
           <div>
-            <label for="dob">Birthdate: </label>
-            <span id="dob">{moment(dob).format("MMMM Do YYYY")}</span>
+            <label htmlFor="dob">Birthdate: </label>
+            <span id="dob">{moment(dob).htmlFormat("MMMM Do YYYY")}</span>
           </div>
         </Skeleton>
       </Card>
